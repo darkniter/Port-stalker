08eaaa12a180a688706cc067f88f0eaa59b733ea
darkniter <Poranot@yandex.ru>
2019-06-21 11:26:09 +0300

    some stage

--- pril/views.py
+++ pril/views.py
@@ -1,9 +1,23 @@
 from pril import app
+from flask import Markup, render_template, request
 
-@app.route('/')
-def index():
-    return 'Index Page'
+@app.route('/', methods=['GET', 'POST'])
+# @app.route('/hello?name=<name>&control=<control>', methods=['GET', 'POST'])
+def hello(name=None, control=None):
+    # Markup('<strong>Hello %s%s%s!</strong>') % ('<blink>',name,'</blink>')
+    if request.method == 'GET':
+        name = request.args.get('name',name)
+        control = request.args.get('control',control)
+    # Markup('<strong>Hello %s%s%s!</strong>') % ('<blink>',name,'</blink>')
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+    # Markup('<em>Marked up</em> &raquo; HTML').striptags()
+
+    return render_template('main.html', name=name, control= control)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login (param_request = []):
+    # if request.method == 'POST':
+    #     return do_the_login()
+    # else:
+    #     return show_the_login_form()
+    return param_request
