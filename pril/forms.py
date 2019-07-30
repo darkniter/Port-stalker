f05fbb99f25a2ea5a43f7d73447674814385ed47
darkniter <Poranot@yandex.ru>
2019-07-30 16:31:32 +0300

    done

--- pril/forms.py
+++ pril/forms.py
@@ -1,5 +1,6 @@
-from wtforms import Form, StringField, TextAreaField, validators
+from wtforms import Form, StringField, TextAreaField, validators, SelectField
 
 class Vendor (Form):
     ip_device = StringField('ip_device',[validators.DataRequired(), validators.Length(min=7,max=15,message='format ip - 255.255.255.255')])
-    vendor_device = StringField('vendor_device', [validators.DataRequired(),validators.Length(min=5,max=5,message='vendor_name = dlink or eltex')])
+    # vendor_device = StringField('vendor_device', [validators.DataRequired(),validators.Length(min=5,max=5,message='vendor_name = dlink or eltex')])
+    list_field = SelectField("list_field", choices=[('default', "-"),('dlink', "Dlink"), ('eltex', "Eltex")])
--- pril/views.py
+++ pril/views.py
@@ -3,19 +3,21 @@ from flask import Markup, render_template, request, redirect, flash, url_for
 
 
 @app.route('/', methods=['GET', 'POST'])
-def reply(name=None, control=None):
-    form = forms.Vendor(request.form)
+def reply():
 
-    ip = request.form.get('ip_device')
-    vendor = request.form.get('vendor_device')
     request_rows=[]
 
-    if ip and vendor:
-
+    if request.method == 'GET':
+        ip = request.args.get('ip')
+        vendor = request.args.get('vendor')
+    elif request.method == 'POST':
+        ip = request.form.get('ip_device')
+        vendor = request.form.get('list_field')
         request_rows = SQLbase.request_SQL(ip,vendor)
-    if request_rows:
-        redirect(url_for('reply', request='?vendor=%s &ip=%s' % (vendor, ip)))
 
+    if vendor:
+        vendor = str.lower(vendor)
 
+    form = forms.Vendor(ip_device=ip, list_field=vendor)
 
-    return render_template('main.html',form=form, ip=ip, vendor = vendor, row = request_rows)
+    return render_template('main.html',form=form, row = request_rows)
