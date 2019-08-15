8507c88785b50e07cbbeb40c93c09cdb0696a6ea
darkniter <Poranot@yandex.ru>
2019-08-15 10:14:03 +0300

    fix name values in views

--- app/pril/views.py
+++ app/pril/views.py
@@ -5,16 +5,16 @@ from prometheus_flask_exporter import PrometheusMetrics
 metrics = PrometheusMetrics(app)
 
 metrics.info('app_info_test', 'Test metrics lib', version='2')
-time_SQL = metrics.info('SQL_request','The response time from the server SQL-base')
-time_Redis = metrics.info('Redis_request','The response time from the server Redis-base')
+time_sql = metrics.info('sql_request_time','The response time from the server sql-base')
+time_redis = metrics.info('redis_request_time','The response time from the server redis-base')
 
-time_SQL.set(0)
+time_sql.set(0)
 
-time_Redis.set(0)
+time_redis.set(0)
 
 @app.route('/', methods=['GET', 'POST'])
 @metrics.do_not_track()
-@metrics.counter('count_call_init', 'Number call init')
+@metrics.counter('client_requests', 'Number call init')
 def reply():
 
     request_rows = []
@@ -29,16 +29,16 @@ def reply():
         ip = request.form.get('ip_device')
         vendor = request.form.get('list_field')
         request_rows, time, header, time_flag = SQLbase.request_SQL(ip, vendor)
-       
+
         if time_flag:
-            time_SQL.set(time)
+            time_sql.set(time)
         else:
-            time_Redis.set(time)
-    
+            time_redis.set(time)
+
     if vendor:
         vendor = str.lower(vendor)
 
-    
-    
+
+
     form = forms.Vendor(ip_device=ip, list_field=vendor)
     return render_template('main.html', form=form, time=time, row=request_rows, header_request=header)
