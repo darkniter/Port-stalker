a1b524a2871711e5d97082499d6290f3cc8c1e61
darkniter <Poranot@yandex.ru>
2019-08-15 12:02:12 +0300

    update metrics for db

--- app/pril/views.py
+++ app/pril/views.py
@@ -1,16 +1,13 @@
 from pril import app, forms, SQLbase
 from flask import render_template, request
-from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_flask_exporter import PrometheusMetrics, Gauge
 
 metrics = PrometheusMetrics(app)
 
-metrics.info('app_info_test', 'Test metrics lib', version='2')
-time_sql = metrics.info('sql_request_time','The response time from the server sql-base')
-time_redis = metrics.info('redis_request_time','The response time from the server redis-base')
-
-time_sql.set(0)
-
-time_redis.set(0)
+metrics.info('app_info', 'radius web app', version='0.1')
+time_db = Gauge('sql_request_time','The response time from the server sql-base',['database'])
+time_db.labels('sql').set(0)
+time_db.labels('redis').set(0)
 
 @app.route('/', methods=['GET', 'POST'])
 @metrics.do_not_track()
@@ -31,9 +28,9 @@ def reply():
         request_rows, time, header, time_flag = SQLbase.request_SQL(ip, vendor)
 
         if time_flag:
-            time_sql.set(time)
+            time_db.labels('sql').set(time)
         else:
-            time_redis.set(time)
+            time_db.labels('redis').set(time)
 
     if vendor:
         vendor = str.lower(vendor)
