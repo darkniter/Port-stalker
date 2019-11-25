516be3a7b33449a8d63fffdadd065bbd6c1026f1
darkniter <Poranot@yandex.ru>
2019-11-25 17:04:56 +0300

    First try

--- client_React/src/index.js
+++ client_React/src/index.js
@@ -1,103 +1,65 @@
-import React from '../node_modules/react';
-import ReactDOM from'../node_modules/react-dom';
+import React from 'react';
+import logo from './assets/logo.png';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-function Square(props){
-  return(
-    <button className = "square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  )
-}
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
 
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
+class AppD extends React.Component {
+  constructor(props){
+    super(props)
     this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
+      firstName: "",
+      data: '',
     };
   }
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i]=this.state.xIsNext ? 'X':'O';
+
+  GetFromBack = event =>
+  {
+    axios.get('http://localhost:5000/guestUser/')
+      .then((res)=>{
+        console.log(res.data.token);
+        this.setState({data: res.data.token})
+      });
+    }
+
+
+  onFirstNameChange = event =>
     this.setState({
-      squares:squares,
-      xIsNext: !this.state.xIsNext,
+      firstName: event.target.value
+    });
+
+  getFromBack(){
+    var res = fetch('http://localhost:5000/guestUser/');
+    this.setState({
+      data: res
     });
   }
 
-  renderSquare(i) {
-    return (<Square
-              value={this.state.squares[i]}
-              onClick={()=>this.handleClick(i)}
-              />
-    );
+  render(){
+        return(
+            // <div>
+            //   <meta charset="utf-8"></meta>
+            //   <link href="./css/bootstrap.min.css" rel="stylesheet"></link>
+            //   <link href="./css/styles.css" rel="stylesheet"></link>
+          <div>
+            <a href="https://yandex.ru/search"><img width="100" align = "center" src={logo} alt="title"></img></a>
+            <br></br><a href='https://yandex.ru/search'>qwe</a>
+            <div>
+              <input type="text" name="firstName" onChange={this.onFirstNameChange} />
+
+              <p>{this.state.firstName}</p>
+              <input type="button" onClick={this.GetFromBack}/>
+              <p>{this.state.data}</p>
+            </div>
+          </div>
+
+        );
+    }
 }
 
-  render() {
-    const status = 'Next step is '+(this.state.xIsNext? 'X':'O');
 
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
 
 ReactDOM.render(
-  <Game />,
+  <AppD />,
   document.getElementById('root')
 );
